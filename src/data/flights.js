af210d581e4f2bac93298ee3768d23ee8ecb2d94
Yuvaraj S <80674658+yrj43@users.noreply.github.com>
2023-02-13 00:37:47 +0530

    fourth commit

---
+++ src/data/flights.js
@@ -0,0 +1,156 @@
+export default {
+  departures: [
+    {
+      id: 'f1',
+      price: 47700,
+      stops: 0,
+      from: {
+        city: 'Sydney',
+        airport: 'SYD',
+        time: '10:30',
+      },
+      to: {
+        city: 'San Francisco',
+        airport: 'SFO',
+        time: '23:40',
+      },
+    },
+    {
+      id: 'f2',
+      price: 57600,
+      stops: 1,
+      from: {
+        city: 'Sydney',
+        airport: 'SYD',
+        time: '13:40',
+      },
+      to: {
+        city: 'San Francisco',
+        airport: 'SFO',
+        time: '12:30',
+      },
+    },
+    {
+      id: 'f3',
+      price: 77400,
+      stops: 2,
+      from: {
+        city: 'Sydney',
+        airport: 'SYD',
+        time: '15:45',
+      },
+      to: {
+        city: 'San Francisco',
+        airport: 'SFO',
+        time: '18:27',
+      },
+    },
+    {
+      id: 'f4',
+      price: 89100,
+      stops: 0,
+      from: {
+        city: 'Sydney',
+        airport: 'SYD',
+        time: '11:50',
+      },
+      to: {
+        city: 'San Francisco',
+        airport: 'SFO',
+        time: '15:27',
+      },
+    },
+    {
+      id: 'f5',
+      price: 98700,
+      stops: 3,
+      from: {
+        city: 'Sydney',
+        airport: 'SYD',
+        time: '9:55',
+      },
+      to: {
+        city: 'San Francisco',
+        airport: 'SFO',
+        time: '15:27',
+      },
+    },
+ {
+      id: 'f6',
+      price: 7400,
+      stops: 2,
+      from: {
+        city: 'Chennai',
+        airport: 'MAS',
+        time: '15:45',
+      },
+      to: {
+        city: 'Trichy',
+        airport: 'TSA',
+        time: '18:27',
+      },
+    },
+  ],
+  returns: [
+    {
+      id: 'r1',
+      price: 56400,
+      stops: 1,
+      from: {
+        city: 'San Francisco',
+        airport: 'SFO',
+        time: '23:40',
+      },
+      to: {
+        city: 'Sydney',
+        airport: 'SYD',
+        time: '8:27',
+      },
+    },
+    {
+      id: 'r2',
+      price: 67000,
+      stops: 0,
+      from: {
+        city: 'San Francisco',
+        airport: 'SFO',
+        time: '17:00',
+      },
+      to: {
+        city: 'Sydney',
+        airport: 'SYD',
+        time: '10:30',
+      },
+    },
+    {
+      id: 'r3',
+      price: 88700,
+      stops: 2,
+      from: {
+        city: 'San Francisco',
+        airport: 'SFO',
+        time: '19:40',
+      },
+      to: {
+        city: 'Sydney',
+        airport: 'SYD',
+        time: '8:40',
+      },
+    },
+    {
+      id: 'r4',
+      price: 102400,
+      stops: 0,
+      from: {
+        city: 'San Francisco',
+        airport: 'SFO',
+        time: '23:40',
+      },
+      to: {
+        city: 'Sydney',
+        airport: 'SYD',
+        time: '10:30',
+      },
+    },
+  ],
+};
