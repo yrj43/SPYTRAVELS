f6c8e3638a1cbd842bd68c574029bb9f189331d3
Yuvaraj S <80674658+yrj43@users.noreply.github.com>
2023-02-28 08:08:15 +0530

    commit

---
+++ precache-manifest.3aefb442b404f303984bf63899793913.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b6bdd15a8e7b25c361e35771a117e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fc3c0300b1a6837af22",
+    "url": "/static/js/0.36289ed6.chunk.js"
+  },
+  {
+    "revision": "4dab9b9ff598010d8998",
+    "url": "/static/js/3.ea8d8d27.chunk.js"
+  },
+  {
+    "revision": "5596529a94486d7b97a6",
+    "url": "/static/js/4.16080015.chunk.js"
+  },
+  {
+    "revision": "051e7cbc19f2dd1d8b8a",
+    "url": "/static/js/5.b0794da3.chunk.js"
+  },
+  {
+    "revision": "f82877d14f6a1eb2c5e2",
+    "url": "/static/js/6.2f3ee679.chunk.js"
+  },
+  {
+    "revision": "42f2853d334ae2980217",
+    "url": "/static/js/main.7156fca8.chunk.js"
+  },
+  {
+    "revision": "8ca6040a06ba7af639ce",
+    "url": "/static/js/runtime-main.19f48bfc.js"
+  },
+  {
+    "revision": "999bd52bb9de0a0d073023c0937f45f7",
+    "url": "/static/media/a320.999bd52b.glb"
+  },
+  {
+    "revision": "3e1a9265f75405434dde16defc6ec0e9",
+    "url": "/static/media/icon-arrow-left.3e1a9265.svg"
+  },
+  {
+    "revision": "445a84bf3807fc8f693836542ee13b41",
+    "url": "/static/media/icon-arrow-right-thin.445a84bf.svg"
+  },
+  {
+    "revision": "2e566cca06aa6ed41ddf23113ee974a5",
+    "url": "/static/media/icon-bag.2e566cca.svg"
+  },
+  {
+    "revision": "40528ada5f463ba9e69f40f085450f97",
+    "url": "/static/media/icon-check.40528ada.svg"
+  },
+  {
+    "revision": "89eb732cb6fbf47b194e7c208ae68022",
+    "url": "/static/media/icon-delete.89eb732c.svg"
+  },
+  {
+    "revision": "3a35c6b776cec9487a3d4add3b0b2ce8",
+    "url": "/static/media/icon-edit.3a35c6b7.svg"
+  },
+  {
+    "revision": "fd2212a9294215bb8e6f6297d309fea2",
+    "url": "/static/media/icon-error.fd2212a9.svg"
+  },
+  {
+    "revision": "3a6bff2dee3a721ea06a3d4e85fb38d2",
+    "url": "/static/media/icon-expand-more.3a6bff2d.svg"
+  },
+  {
+    "revision": "b4e35e734ea357c1f7c4fc60c3b6c72e",
+    "url": "/static/media/icon-flight.b4e35e73.svg"
+  },
+  {
+    "revision": "81bec562c5e0d3ea39d8100c4f1b0ce2",
+    "url": "/static/media/icon-location.81bec562.svg"
+  },
+  {
+    "revision": "2d7800e3ed5517c37ebafe684333a359",
+    "url": "/static/media/logo-wordmark.2d7800e3.svg"
+  },
+  {
+    "revision": "a5eea1170830eb270e4079ee97ee3e10",
+    "url": "/static/media/logo.a5eea117.svg"
+  },
+  {
+    "revision": "38ebdf67297c01ad5e0a309130940779",
+    "url": "/static/media/plane-type.38ebdf67.svg"
+  },
+  {
+    "revision": "c78a79932aa76fe49421b0d800f3815c",
+    "url": "/static/media/qr-code.c78a7993.svg"
+  }
+]);
